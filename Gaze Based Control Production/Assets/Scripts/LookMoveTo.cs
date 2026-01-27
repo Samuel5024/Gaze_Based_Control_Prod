6ca59e7fc2f5b313c79f970c1cf144af9cdcf03f
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-01-26 23:43:37 -0700

    update LookMoveTo.cs

--- Gaze Based Control Production/Assets/Scripts/LookMoveTo.cs
+++ Gaze Based Control Production/Assets/Scripts/LookMoveTo.cs
@@ -5,20 +5,21 @@ public class LookMoveTo : MonoBehaviour
 {
     public GameObject ground;
 
-    // Update is called once per frame
     void Update()
     {
         Transform camera = Camera.main.transform;
         Ray ray;
-        RaycastHit hit;
+        RaycastHit[] hits;
         GameObject hitObject;     
 
         Debug.DrawRay(camera.position, camera.rotation * Vector3.forward * 100.0f);
 
         ray = new Ray(camera.position, camera.rotation * Vector3.forward);
 
-        if(Physics.Raycast(ray, out hit))
+        hits = Physics.RaycastAll(ray);
+        for(int i = 0; i < hits.Length; i++)
         {
+            RaycastHit hit = hits[i];
             hitObject = hit.collider.gameObject;
             if(hitObject == ground)
             {
